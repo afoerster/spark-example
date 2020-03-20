5887b02fb038e1191b8e1f825189ea8441ac1ee2
Tony Foerster <anthonymfoerster@gmail.com>
2020-03-20 11:43:32 -0500

    Add data objects

---
+++ src/main/scala/com/spark/example/assignment1/Station.scala
@@ -0,0 +1,9 @@
+package com.spark.example.assignment1
+
+case class Station(station_id: Int,
+                   name: String,
+                   lat: Double,
+                   lon: Double,
+                   dockcount: Int,
+                   landmark: String,
+                   installation: String)
---
+++ src/main/scala/com/spark/example/assignment1/Trip.scala
@@ -0,0 +1,13 @@
+package com.spark.example.assignment1
+
+case class  Trip(trip_id: String,
+                duration: Long,
+                start_date: String,
+                start_station: String,
+                start_terminal: Int,
+                end_date: String,
+                end_station: String,
+                end_terminal: Int,
+                bike_number: Int,
+                subscriber_type: String,
+                zip_code: String)
